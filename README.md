a95e00ef09bee319fd34c9ecc5151d3852c77635
anderuraga <ander.uraga@gmail.com>
2018-02-22 16:37:36 +0100

    solucionado funciones comunes

---
+++ README.md
@@ -0,0 +1,6 @@
+# letrakApp
+
+App realizada en ionic para que niñ@s puedan aprender las vocales y letras.
+
+
+![Alt text](https://github.com/anderuraga/letrakApp/blob/master/screenshoots/screenshot1.png)
--- src/app/comun.ts
+++ src/app/comun.ts
@@ -1,6 +1,6 @@
 export class Comun {
 
-    getRandomInt(min, max) {
+    static getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
@@ -8,7 +8,7 @@ export class Comun {
    * Randomize array element order in-place.
    * Using Durstenfeld shuffle algorithm.
    */
-  shuffleArray(array) {
+  static shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
